dff8f2c0b4d911a64314b18434a9dfc3fb81d505
Mengheng ENG <mengheng.eng@student.cadt.edu.kh>
2026-06-22 23:15:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+#include <accctrl.h>
 #include <stdio.h>
+#include "display1st.h"
+#include "alert.h"
+#include "product.h"
+#include "stockmanagement.h"
 #include "data.h"
 
-int main(void)
+int main()
 {
-printf ( "current pro. %d",);
+    allocateallStocks(); // split stock into online and physical on startup
+    display_login();
+
+    return 0;
 }
