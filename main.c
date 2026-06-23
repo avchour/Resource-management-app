e908dc45fa5e011e3244cff717a8a689dcee63fa
Mengheng ENG <mengheng.eng@student.cadt.edu.kh>
2026-06-23 13:38:47 +0700

    fix

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include "display1st.h"
 #include "alert.h"
+#include "data.h"
+#include "display1st.h"
+#include "finance.h"
+#include "onlinePurchase.h"
+#include "physicalPurchase.h"
 #include "product.h"
 #include "stockmanagement.h"
-#include "data.h"
+#include "restock.h"
+
 
 int main()
 {
---
+++ utils.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int getIntInput(const char *prompt)
+{
+    int value;
+
+    while (1)
+    {
+        printf("%s", prompt);
+
+        if (scanf("%d", &value) == 1)
+        {
+            while (getchar() != '\n')
+                ;
+            return value;
+        }
+
+        printf("Invalid input! Please enter a number.\n");
+
+        while (getchar() != '\n')
+            ;
+    }
+}
